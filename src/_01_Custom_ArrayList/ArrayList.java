a1cc36ef2233fca1f811edbd5ceb40d2ba82ccb4
jscornick <56415689+jscornick@users.noreply.github.com>
2021-03-21 16:29:43 -0700

    Starting ArrayList Module 1

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -2,34 +2,35 @@ package _01_Custom_ArrayList;
 
 @SuppressWarnings("unchecked")
 
-public class ArrayList <T>{
-	
+public class ArrayList<T> {
+	T[] memberArr;
+
 	public ArrayList() {
+		memberArr = (T[]) new Object[0];
 	}
-	
+
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+
+		return memberArr[loc];
 	}
-	
 	public void add(T val) {
 		
 	}
-	
+
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public void remove(int loc) throws IndexOutOfBoundsException {
-		
+
 	}
-	
+
 	public boolean contains(T val) {
-		
+
 		return false;
 	}
 }
